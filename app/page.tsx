4d4d33f3b69df617666a02c2e811433d8e6964c3
lopezkor000 <koriellopez03@gmail.com>
2026-01-05 00:08:06 -0600

    homepage progress, hero added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
 import AngledRectangle from "@/app/components/AngledRectangle/AngledRectangle";
+import styles from "./page.module.css";
 
 export default function Home() {
 	return (
-		<div></div>
+		<div>
+      <div className={styles.container}>
+        <img src="hero.jpg"/>
+        <a target="_blank" href="https://ieee.org" className={`${styles.centered_link} ${styles.container}`}>
+          <img src="/ieee_mb_white.png" />
+        </a>
+      </div>
+      <AngledRectangle>
+        <h1>About Us</h1>
+        <p>
+          The Institute of Electrical and Electronics Engineers - Edinburg Student Branch (IEEE ESB) is a dynamic, student-run organization dedicated to fostering innovation, leadership, and hands-on experience in engineering and technology.
+        </p>
+        <button>Learn More</button>
+      </AngledRectangle>
+    </div>
 	);
 }
