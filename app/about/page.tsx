4cb3c67add9d152fd1c57e86f852db513c823b5d
Kory <74568018+lopezkor000@users.noreply.github.com>
2026-02-07 16:37:25 -0600

    Color contrast

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ import styles from "./page.module.css";
 export default function About() {
 	return (
 		<div>
-			<AngledRectangle color="yellow" textColor="white">
+			<AngledRectangle color="yellow" textColor="blue">
 				<div className={styles.aboutContainer}>
 					<div>
 						<h1>About Us</h1>
